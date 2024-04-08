3a06945b880d4147bdd01fafde67fbd25e8d7949
Dovydas Štrupaitis <45108602+ValIIDev@users.noreply.github.com>
2024-04-08 18:21:36 +0300

    Add files via upload
    backup

---
+++ Minigame/Minigame/Form1.Designer.vb
@@ -0,0 +1,107 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        PictureBox1 = New PictureBox()
+        PictureBox2 = New PictureBox()
+        PictureBox3 = New PictureBox()
+        Button1 = New Button()
+        Label1 = New Label()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox3, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Image = My.Resources.Resources.O
+        PictureBox1.Location = New Point(44, 52)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(124, 186)
+        PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+        PictureBox1.TabIndex = 0
+        PictureBox1.TabStop = False
+        ' 
+        ' PictureBox2
+        ' 
+        PictureBox2.Image = My.Resources.Resources.O
+        PictureBox2.Location = New Point(204, 52)
+        PictureBox2.Name = "PictureBox2"
+        PictureBox2.Size = New Size(124, 186)
+        PictureBox2.SizeMode = PictureBoxSizeMode.StretchImage
+        PictureBox2.TabIndex = 1
+        PictureBox2.TabStop = False
+        ' 
+        ' PictureBox3
+        ' 
+        PictureBox3.Image = My.Resources.Resources.O
+        PictureBox3.Location = New Point(363, 52)
+        PictureBox3.Name = "PictureBox3"
+        PictureBox3.Size = New Size(124, 186)
+        PictureBox3.SizeMode = PictureBoxSizeMode.StretchImage
+        PictureBox3.TabIndex = 2
+        PictureBox3.TabStop = False
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(44, 12)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 3
+        Button1.Text = "Start Game"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(447, 12)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(40, 15)
+        Label1.TabIndex = 4
+        Label1.Text = "Points"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(556, 311)
+        Controls.Add(Label1)
+        Controls.Add(Button1)
+        Controls.Add(PictureBox3)
+        Controls.Add(PictureBox2)
+        Controls.Add(PictureBox1)
+        Name = "Form1"
+        Text = "Form1"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox3, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Minigame/Minigame/Form1.vb
@@ -0,0 +1,71 @@
+﻿Public Class Form1
+    Dim ImpostNumber As Int16
+    Dim points As Int32
+    Dim Randimage As Int16
+    Dim Rand As Random = New Random
+
+    Function PictureRandomizer()
+        Randimage = Rand.Next(1, 6)
+        If Randimage = 1 Then
+            PictureBox1.Image = My.Resources.B
+        End If
+        If Randimage = 2 Then
+            PictureBox1.Image = My.Resources.BL
+        End If
+        If Randimage = 3 Then
+            PictureBox1.Image = My.Resources.Y
+        End If
+        If Randimage = 4 Then
+            PictureBox1.Image = My.Resources.O
+        End If
+        If Randimage = 5 Then
+            PictureBox1.Image = My.Resources.P
+        End If
+        'end code for image1
+
+        ''Code for image 2
+        Randimage = Rand.Next(1, 6)
+        If Randimage = 1 Then
+            PictureBox2.Image = My.Resources.B
+        End If
+        If Randimage = 2 Then
+            PictureBox2.Image = My.Resources.BL
+        End If
+        If Randimage = 3 Then
+            PictureBox2.Image = My.Resources.Y
+        End If
+        If Randimage = 4 Then
+            PictureBox2.Image = My.Resources.O
+        End If
+        If Randimage = 5 Then
+            PictureBox2.Image = My.Resources.P
+        End If
+        'end code for image2
+
+        ''Code for image 2
+        Randimage = Rand.Next(1, 6)
+        If Randimage = 1 Then
+            PictureBox3.Image = My.Resources.B
+        End If
+        If Randimage = 2 Then
+            PictureBox3.Image = My.Resources.BL
+        End If
+        If Randimage = 3 Then
+            PictureBox3.Image = My.Resources.Y
+        End If
+        If Randimage = 4 Then
+            PictureBox3.Image = My.Resources.O
+        End If
+        If Randimage = 5 Then
+            PictureBox3.Image = My.Resources.P
+        End If
+        'end code for image2
+    End Function
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ImpostNumber = Rand.Next(1, 4)
+        PictureRandomizer()
+
+    End Sub
+End Class
